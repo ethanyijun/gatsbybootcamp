d03c106858a6666453f2bf41dfd00ebb68000d10
yijungai <ethan.yijun@gmail.com>
2020-05-04 22:13:35 +1000

    Add authentication feature

--- Trips/ClientApp/src/components/NavMenu.js
+++ Trips/ClientApp/src/components/NavMenu.js
@@ -2,45 +2,38 @@ import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
+import {useAuth0} from '../react-auth0-spa';
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
-
-  constructor (props) {
-    super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  render () {
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <NavbarBrand tag={Link} to="/">Trips</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/create">Create</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/trips">Trips</NavLink>
-                </NavItem>
-              </ul>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    );
-  }
+const NavMenu = () => {
+  const {isAuthenticated, loginWithRedirect, logout} = useAuth0();
+  return (
+    <header>
+      <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+        <Container>
+          <NavbarBrand tag={Link} to="/">Trips</NavbarBrand>
+          {isAuthenticated ? (
+            <ul className="navbar-nav flex-grow">
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/create">Create</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/trips">Trips</NavLink>
+              </NavItem>
+              <NavItem>
+                <button className="btn btn-danger" onClick={() => logout()}>Log out</button>
+              </NavItem>
+            </ul>
+          ):(
+            <ul className="navbar-nav flex-grow">
+              <NavItem>
+                <button className="btn btn-success" onClick={() => loginWithRedirect()}>Log in</button>
+              </NavItem>
+            </ul>
+          )}
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
+
+export default NavMenu;
--- Trips/ClientApp/src/index.js
+++ Trips/ClientApp/src/index.js
@@ -4,19 +4,37 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Auth0Provider} from './react-auth0-spa';
+import config from './auth_config.json'
 import {Provider} from 'react-redux';
 import configureStore from './store/store';
 
+const store = configureStore({})
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
-const store = configureStore({})
+
+const onRedirectCallback = appState => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl ? appState.targetUrl
+    : window.location.pathname
+  );
+};
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter basename={baseUrl}>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}>
+    <Provider store={store}>
+      <BrowserRouter basename={baseUrl}>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </Auth0Provider>
+  ,
   rootElement);
 
 registerServiceWorker();
